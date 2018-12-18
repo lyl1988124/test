a369dce38975df6a714a1e7603b0aa180f3773f0
liuyuanlong <lyl@didiglobal.com>
2018-12-18 21:13:57 +0800

    new file

---
+++ src/main/java/com/lyl/designPattern/prototype/Rectangle.java
@@ -0,0 +1,16 @@
+package com.lyl.designPattern.prototype;
+
+import java.lang.reflect.Type;
+
+/**
+ * Created by lyl
+ * Date 2018/12/18 10:04
+ */
+public class Rectangle extends Shape {
+
+
+    @Override
+    void draw() {
+        System.out.println("draw Rectangle");
+    }
+}
