ab90e00bc89345f32beac12faa6a37eaa6431d54
liuyuanlong <lyl@didiglobal.com>
2018-12-18 21:46:03 +0800

    new file

--- src/main/java/com/lyl/designPattern/prototype/PrototypePatternDemo.java
+++ src/main/java/com/lyl/designPattern/prototype/PrototypePatternDemo.java
@@ -9,6 +9,13 @@ public class PrototypePatternDemo {
     public static void main(String[] args) {
         ShapeCache.loadCache();
         Shape clonedShape = (Shape) ShapeCache.getShape("1");
+        Shape clonedShape2 = (Shape) ShapeCache.getShape("1");
+        System.out.println(clonedShape.getClass()==clonedShape2.getClass());
         System.out.println("Shape : " + clonedShape.getType());
+
+        MyPrototype myPrototype = new MyPrototype();
+        myPrototype.setName(" aaa");
+
+        myPrototype.dosplay();
     }
 }
