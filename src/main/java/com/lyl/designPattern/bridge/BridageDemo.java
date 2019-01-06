d595c59d86732eb6994ddcb9d90ce67a79c361be
liuyuanlong <lyl@didiglobal.com>
2019-01-06 14:30:42 +0800

    test

---
+++ src/main/java/com/lyl/designPattern/bridge/BridageDemo.java
@@ -0,0 +1,12 @@
+package com.lyl.designPattern.bridge;
+
+/**
+ * Created by lyl
+ * Date 2019/1/6 14:16
+ */
+public class BridageDemo {
+    public static void main(String[] args) {
+        Shape redCircle = new Circle(1,2,3,new RedCircle());
+        redCircle.draw();
+    }
+}
---
+++ src/main/java/com/lyl/designPattern/bridge/DrawAPI.java
@@ -0,0 +1,9 @@
+package com.lyl.designPattern.bridge;
+
+/**
+ * Created by lyl
+ * Date 2019/1/6 13:46
+ */
+public interface DrawAPI {
+    public void drawCircle();
+}
