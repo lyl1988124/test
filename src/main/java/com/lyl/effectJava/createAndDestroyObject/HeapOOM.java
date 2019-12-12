997ac01f45844746dc1d71249eed0733604265cf
liuyuanlong <lyl@didiglobal.com>
2019-12-12 19:53:20 +0800

    新增 GC

---
+++ src/main/java/com/lyl/effectJava/createAndDestroyObject/HeapOOM.java
@@ -0,0 +1,26 @@
+package com.lyl.effectJava.createAndDestroyObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lyl
+ * Date 2019/12/11 19:47
+ *
+ * -verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8 -XX:+HeapDumpOnOutOfMemoryError
+ * -Xms20M -Xmx20M -XX:+HeapDumpOnOutOfMemoryError
+ *
+ * 1. -Xms20M -Xmx20M -XX:+HeapDumpOnOutOfMemoryError
+ */
+public class HeapOOM {
+    static class OOMObject{
+
+    }
+
+    public static void main(String[] args) {
+        List<OOMObject> list = new ArrayList<>();
+        while (true){
+            list.add(new OOMObject());
+        }
+    }
+}
---
+++ src/main/java/com/lyl/effectJava/createAndDestroyObject/YongGenGC.java
@@ -0,0 +1,9 @@
+package com.lyl.effectJava.createAndDestroyObject;
+
+/**
+ * Created by lyl
+ * Date 2019/12/11 19:47
+ */
+public class YongGenGC {
+
+}
