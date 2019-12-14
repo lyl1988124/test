29539eb34da4493028185596136718e15701dc63
liuyuanlong <lyl@didiglobal.com>
2019-12-14 11:56:57 +0800

    新增 GC

---
+++ src/main/java/com/lyl/effectJava/createAndDestroyObject/JavaVMStackSOF.java
@@ -0,0 +1,25 @@
+package com.lyl.effectJava.createAndDestroyObject;
+
+/**
+ * Created by lyl
+ * Date 2019/12/12 19:54
+ * VM Args : -Xss128k
+ */
+public class JavaVMStackSOF {
+    private int stackLength = 1;
+
+    public void stackLeak(){
+        stackLength++;
+        stackLeak();
+    }
+
+    public static void main(String[] args) {
+        JavaVMStackSOF oom = new JavaVMStackSOF();
+        try {
+            oom.stackLeak();
+        } catch (Throwable e) {
+            System.out.println("stack length:" + oom.stackLength);
+            e.printStackTrace();
+        }
+    }
+}
