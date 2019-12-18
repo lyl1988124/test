c349296fa323939efcb919737e47a44a6379d6c9
liuyuanlong <lyl@didiglobal.com>
2019-12-18 20:45:35 +0800

    新增 GC

---
+++ src/main/java/com/lyl/effectJava/createAndDestroyObject/ReferejceCountingGC.java
@@ -0,0 +1,25 @@
+package com.lyl.effectJava.createAndDestroyObject;
+
+/**
+ * Created by lyl
+ * Date 2019/12/14 15:24
+ */
+public class ReferejceCountingGC {
+    public Object instance = null;
+    private static final int _1MB = 1024 * 1024;
+    private byte[] bigSize = new byte[2 * _1MB];
+
+//    public static void testGC(){
+public static void main(String[] args) {
+        ReferejceCountingGC objA = new ReferejceCountingGC();
+        ReferejceCountingGC objB = new ReferejceCountingGC();
+        objA.instance = objB;
+        objB.instance = objA;
+
+        objA = null;
+        objB = null;
+
+        System.gc();
+
+    }
+}
