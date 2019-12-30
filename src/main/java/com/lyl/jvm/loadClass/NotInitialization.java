1ac3601365c3add00bfbf10e9683f88f294c1f57
liuyuanlong <lyl@didiglobal.com>
2019-12-30 20:19:48 +0800

    新增

---
+++ src/main/java/com/lyl/jvm/loadClass/NotInitialization.java
@@ -0,0 +1,15 @@
+package com.lyl.jvm.loadClass;
+
+import com.sun.tools.internal.jxc.ap.Const;
+
+/**
+ * Created by lyl
+ * Date 2019/12/27 19:58
+ */
+public class NotInitialization {
+    public static void main(String[] args) {
+        //System.out.println(SubClass.value);
+        //SuperClass[] sca = new SubClass[10];
+        System.out.println(ConstClass.HELLOWORLD);
+    }
+}
