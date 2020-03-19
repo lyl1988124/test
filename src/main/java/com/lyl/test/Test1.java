779f77521e77226895d8b061cce26b628107a9eb
lyl_pc <lyl_pc@lyl>
2020-03-19 19:16:16 +0800

    new

--- src/main/java/com/lyl/test/Test1.java
+++ src/main/java/com/lyl/test/Test1.java
@@ -8,19 +8,31 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.IntStream;
 
 public class Test1 {
 
-    public static void main(String[] args) throws InterruptedException {
-
-        LocalDate today = LocalDate.now();
-
-        System.out.println(LocalDate.now());
-
-
-
-        System.out.println(LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm")));
+    public static void main(String[] args) {
+        int num=705;
+        System.out.println(convert(num));
+    }
+    public static String convert(int num){
+        int size=26;
+        StringBuffer sb=new StringBuffer();
+        boolean have=true;
+        while(have){
+            sb.insert(0,(char)((num-1)%size+97));
+            num=(num-1)/size;
+            if (num==0){
+                have=false;
+            }
+        }
+        Map<String,String> map = new Hashtable();
+        Map<String,String> map2 = new HashMap();
+        return sb.toString();
     }
 }
